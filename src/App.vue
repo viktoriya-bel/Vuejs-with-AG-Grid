4b9ec995f9017d61a282d99939e4d5698365c063
Viki <avocado-dev@yandex.ru>
2025-11-25 20:44:15 +0400

    add data table

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <ag-grid-vue
+    :rowData="rowData"
+    :columnDefs="columnDefs"
+    style="height: 500px"
+    :defaultColDef="defaultColDef"
+    :statusBar="statusBar"
+  >
+  </ag-grid-vue>
 </template>
+
+<script setup lang="ts">
+import { AgGridVue } from 'ag-grid-vue3'
+
+import { AllEnterpriseModule, ColDef, LicenseManager, ModuleRegistry } from 'ag-grid-enterprise'
+import { Data } from './type'
+ModuleRegistry.registerModules([AllEnterpriseModule])
+LicenseManager.setLicenseKey('<your license key>')
+
+const columnDefs: ColDef<Data>[] = [
+  { headerName: '№ п/п', field: 'id' },
+  { headerName: 'Категория', field: 'parent' },
+  { headerName: 'Наименование', field: 'label' },
+]
+
+const rowData: Data[] = [
+  { id: 1, parent: null, label: 'Айтем 1' },
+  { id: '91064cee', parent: 1, label: 'Айтем 2' },
+  { id: 3, parent: 1, label: 'Айтем 3' },
+
+  { id: 4, parent: '91064cee', label: 'Айтем 4' },
+  { id: 5, parent: '91064cee', label: 'Айтем 5' },
+  { id: 6, parent: '91064cee', label: 'Айтем 6' },
+
+  { id: 7, parent: 4, label: 'Айтем 7' },
+  { id: 8, parent: 4, label: 'Айтем 8' },
+]
+
+const defaultColDef = {
+  flex: 1,
+}
+
+const statusBar = {}
+</script>
 
 <style scoped></style>
---
+++ src/type.ts
@@ -0,0 +1,5 @@
+export interface Data {
+  id: number | string
+  parent: number | string | null
+  label?: string
+}
